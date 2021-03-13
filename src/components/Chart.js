833824b52d2c7d10249507fbb4a26ad96322198c
Julien Aubert <julien.aubert.rbs@gmail.com>
2021-03-13 12:06:55 +0100

    Installed dependencies, created first chart component, display example chart on homepage

---
+++ src/components/Chart.js
@@ -0,0 +1,31 @@
+import Chart from 'react-apexcharts';
+
+const ChartHome = () => {
+  const data = {
+    options: {
+      chart: {
+        id: 'apexchart-example',
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+    },
+    series: [
+      {
+        name: 'series-1',
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+    ],
+  };
+  return (
+    <Chart
+      options={data.options}
+      series={data.series}
+      type="bar"
+      width={500}
+      height={320}
+    />
+  );
+};
+
+export default ChartHome;
